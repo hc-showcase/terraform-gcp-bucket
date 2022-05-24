582424dead920d8fb2ad540638628880c0c3042e
Marc-Steffen Kaesz <mskaesz@protonmail.com>
2022-05-24 20:02:04 +0200

    init

---
+++ main.tf
@@ -0,0 +1,30 @@
+variable "bucket_name" {
+   description = "The name of the bucket."
+}
+
+resource "google_storage_bucket" "bucket" {
+  name          = var.bucket_name
+  location      = "EU"
+  force_destroy = true
+}
+
+resource "google_service_account" "bucket" {
+  account_id   = "mkaesz-bucket"
+  display_name = "GCS Bucket"
+}
+
+resource "google_service_account_key" "bucket" {
+  service_account_id = google_service_account.bucket.name
+}
+
+resource "google_storage_bucket_iam_member" "member-object" {
+  bucket = google_storage_bucket.bucket.name
+  role   = "roles/storage.objectAdmin"
+  member = "serviceAccount:${google_service_account.bucket.email}"
+}
+
+resource "google_storage_bucket_iam_member" "member-bucket" {
+  bucket = google_storage_bucket.bucket.name
+  role   = "roles/storage.legacyBucketReader"
+  member = "serviceAccount:${google_service_account.bucket.email}"
+}
